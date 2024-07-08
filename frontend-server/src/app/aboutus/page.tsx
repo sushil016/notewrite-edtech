5832be2bf7f89f5f5fcacafff52cda72b1648d01
sushil016 <sahanisushil325@gmail.com>
2024-07-08 23:42:56 +0530

    about us page

---
+++ frontend-server/src/app/aboutus/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='w-full min-h-screen flex '>
+        <div>
+           <h1>Know About our team</h1>
+        </div>
+    </main>
+  )
+}
+
+export default page
